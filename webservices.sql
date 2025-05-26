01f5c479950df79bbfedd28706f736ceb8fa2c4f
anzelinux <155091432+anzelinux@users.noreply.github.com>
2025-05-26 17:15:20 +0200

    Update webservices.sql

--- webservices.sql
+++ webservices.sql
@@ -7,3 +7,36 @@ create table cliente (
   contatti varchar(30) not null,
   azienda varchar(30) not null,
   primary key (id_cliente));
+
+
+create table stato (
+	id_stato_consulenza smallint not null,
+	stato varchar(10),
+	primary key(id_stato_consulenza));
+
+create table consulenza (
+	id_consulenza int not null AUTO_INCREMENT,
+	data_creazione date , 
+	check (data_creazione >= '2025-01-01'),
+	note varchar(200) null,
+	id_stato_consulenza smallint not null,
+	id_cliente smallint not null,
+	primary key (id_consulenza),
+	foreign key(id_stato_consulenza) references stato(id_stato_consulenza),
+	foreign key(id_cliente) references cliente(id_cliente)
+);
+
+create table contratto (
+	id_contratto smallint not null AUTO_INCREMENT,
+	data_inizio date not null,
+	check (data_inizio>= '2025-01-01'),
+	data_fine date null,
+	check (data_fine >= data_inizio),
+	id_servizio smallint,
+	id_consulenza int not null,
+
+	primary key (id_contratto),
+	foreign key (id_consulenza) references consulenza(id_consulenza),
+	foreign key (id_servizio) references servizio (id_servizio)
+
+);
