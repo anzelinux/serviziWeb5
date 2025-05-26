8c4bc86104babcd3cf54692cf42aa7362c46b7c2
anzelinux <155091432+anzelinux@users.noreply.github.com>
2025-05-26 07:00:31 +0200

    Update webservices.sql

--- webservices.sql
+++ webservices.sql
@@ -1,1 +1,9 @@
+--create database webservices;
 
+use webservices;
+create table cliente (
+  id_cliente smallint AUTO_INCREMENT not null,
+  nome varchar(30) not null,
+  contatti varchar(30) not null,
+  azienda varchar(30) not null,
+  primary key (id_cliente));
